4149172c46810e198863fbc9216e8fabafd80c90
ignazweis2 <213210679+ignazweis2@users.noreply.github.com>
2025-08-05 21:55:44 -0300

    adicionado mais código

--- jogo-da-velha/jogo-da-velha.c
+++ jogo-da-velha/jogo-da-velha.c
@@ -2,9 +2,10 @@
 tornando-o o mais eficiente. Não está completo e não é jogável.*/
 
 #include <stdio.h>
+#include <string.h>
 
-void player_position(player);
-int calc(void);
+void player_position(char player, char buff[], int i, char n[][3], char p[])
+int calc(char p)
 
 int main (void)
 {
@@ -20,14 +21,15 @@ int main (void)
             p[i] = '.';
     }
 
-    while (end)
-    {
-        // escolha o jogador.
-        printf("Qual jogador escolhido primeiro? o\\x\n");
-        printf(": ");
-        char player[2];
-        scanf("%1s", &player);
+    // escolha o jogador.
+    printf("Qual jogador escolhido primeiro? o\\x\n");
+    printf(": ");
+    char player[2];
+    scanf("%1s", &player);
 
+    while (0)
+    {
+        // mostre o jogo da velha
         printf("%c %c %c\n", p[0], p[1], p[2]);
         printf("%c %c %c\n", p[3], p[4], p[5]);
         printf("%c %c %c\n", p[6], p[7], p[8]);
@@ -35,36 +37,59 @@ int main (void)
         printf("\n1a 1b 1c\n2a 2b 2c\n3a 3b 3c\n");
         printf(": ");
 
+        // coloque a opção no buffer
         char buff[3];
-        scanf("%2s", &player);
-        
-        end = calc();
-    }
-}
+        scanf("%2s", &buff);
 
-int calc(void)
-{  
-    if ((p[0] == player && p[1] == player && p[2] == player) ||
-        (p[3] == player && p[4] == player && p[5] == player) ||
-        (p[6] == player && p[7] == player && p[8] == player) ||
-
-        (p[0] == player && p[3] == player && p[6] == player) ||
-        (p[1] == player && p[4] == player && p[7] == player) ||
-        (p[2] == player && p[5] == player && p[8] == player) ||
-
-        (p[0] == player && p[4] == player && p[8] == player) ||
-        (p[6] == player && p[4] == player && p[2] == player))
+        // mude o jogo da velha para refletir a opção dada anteriormente
+        char n[9][3] = {"1a","1b","1c","2a","2b","2c","3a","3b","3c"};
+        for (i = 0; i < 9; i++)
         {
-            return 1;
-        } 
+            player_position(player, buff, i, n, p);
+        }
+
+        //caso alguém ganhe finalize o while com a variável end
+        end = calc(p[]);
+
+        if (end == 1)
+        {
+            printf("%c ganhou!");
+            break;
+        }
+        else if (move == 8)
+        {
+            printf("\n\nVocê empatou!\n");
+            break;
+        }
+
+        move++;
+    }
 }
 
-void player_position(player, buff)
+int calc(char p)
 {
-    char n[9][3] = {"1a","1b","1c","2a","2b","2c","3a","3b","3c"}
-    for (i = 0; i < 9; i++)
+    if (move = 4)
     {
-        if (n[i] == buff) {p[i] = player;}
-    }
+        if ((p[0] == player && p[1] == player && p[2] == player) ||
+            (p[3] == player && p[4] == player && p[5] == player) ||
+            (p[6] == player && p[7] == player && p[8] == player) ||
 
+            (p[0] == player && p[3] == player && p[6] == player) ||
+            (p[1] == player && p[4] == player && p[7] == player) ||
+            (p[2] == player && p[5] == player && p[8] == player) ||
+
+            (p[0] == player && p[4] == player && p[8] == player) ||
+            (p[6] == player && p[4] == player && p[2] == player))
+            {
+                return 1;
+            }
+    }
+}
+
+void player_position(char player, char buff[], int i, char n[][3], char p[])
+{
+    if (strcmp(n[i], buff) == 0)
+    {
+        p[i] = player;
+    }
 }
