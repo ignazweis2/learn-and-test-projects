9d9f6063da934cfde555e1950379c03e2ad8aa95
ignazweis2 <213210679+ignazweis2@users.noreply.github.com>
2025-08-06 18:28:21 -0300

    adicionado mais código quase a ponto de funcionamento completo

--- jogo-da-velha/jogo-da-velha.c
+++ jogo-da-velha/jogo-da-velha.c
@@ -4,16 +4,15 @@ tornando-o o mais eficiente. Não está completo e não é jogável.*/
 #include <stdio.h>
 #include <string.h>
 
-void player_position(char player, char buff[], int i, char n[][3], char p[])
-int calc(char p[], move)
+int calc(char p[], int move, char player[]);
 
 int main (void)
 {
     // váriaveis criadas na memória.
-    player[2] = {'X','O'};
+    char p[9];
     int end = 0;
     int limit = 0;
-    char p[9];
+    int move = 0;
     
     // adiciona um ponto em cada posição do jogo da velha.
     for (int i = 0; i < 9; i++)
@@ -27,9 +26,16 @@ int main (void)
     char player[2];
     scanf("%1s", &player);
 
-    while (0)
+    // verificar se escolha do jogador é válida
+    if (player[0] != 'x' && player[0] != 'o')
     {
-        // mostre o jogo da velha
+        printf("\nJogador inválido.\n\n");
+        return 1;
+    }
+
+    while (true)
+    {
+        // mostre o jogo da velha.
         printf("%c %c %c\n", p[0], p[1], p[2]);
         printf("%c %c %c\n", p[3], p[4], p[5]);
         printf("%c %c %c\n", p[6], p[7], p[8]);
@@ -37,23 +43,27 @@ int main (void)
         printf("\n1a 1b 1c\n2a 2b 2c\n3a 3b 3c\n");
         printf(": ");
 
-        // coloque a opção no buffer
+        // coloque a opção no buffer.
         char buff[3];
         scanf("%2s", &buff);
 
-        // mude o jogo da velha para refletir a opção dada anteriormente
+        // mude o jogo da velha para refletir a opção dada anteriormente.
         char n[9][3] = {"1a","1b","1c","2a","2b","2c","3a","3b","3c"};
-        for (i = 0; i < 9; i++)
+        for (int i = 0; i < 9; i++)
         {
-            player_position(player, buff, i, n, p);
+            if (strcmp(n[i], buff) == 0 && p[i] == '.')
+            {
+                p[i] = player[0];
+            }
         }
 
-        //caso alguém ganhe finalize o while com a variável end
-        end = calc(p[], move);
+        // caso alguém ganhe finalize o while com a variável end.
+        end = calc(p, move, player);
 
+        // verificação se alguém ganhou ou perdeu.
         if (end == 1)
         {
-            printf("%c ganhou!");
+            printf("\n\n%c ganhou!\n", player[0]);
             break;
         }
         else if (move == 8)
@@ -62,34 +72,38 @@ int main (void)
             break;
         }
 
+        // adicionar a quantidade de rodadas
         move++;
+
+        // trocar o jogador
+        if (player[0] == 'x')
+        {
+            player[0] = 'o';
+        }
+        else if (player[0] == 'o')
+        {
+            player[0] = 'x';
+        }
     }
 }
 
-int calc(char p[], move)
+int calc(char p[], int move, char player[])
 {
-    if (move = 4)
+    if (move >= 4)
     {
-        if ((p[0] == player && p[1] == player && p[2] == player) ||
-            (p[3] == player && p[4] == player && p[5] == player) ||
-            (p[6] == player && p[7] == player && p[8] == player) ||
+        if ((p[0] == player[0] && p[1] == player[0] && p[2] == player[0]) ||
+            (p[3] == player[0] && p[4] == player[0] && p[5] == player[0]) ||
+            (p[6] == player[0] && p[7] == player[0] && p[8] == player[0]) ||
 
-            (p[0] == player && p[3] == player && p[6] == player) ||
-            (p[1] == player && p[4] == player && p[7] == player) ||
-            (p[2] == player && p[5] == player && p[8] == player) ||
+            (p[0] == player[0] && p[3] == player[0] && p[6] == player[0]) ||
+            (p[1] == player[0] && p[4] == player[0] && p[7] == player[0]) ||
+            (p[2] == player[0] && p[5] == player[0] && p[8] == player[0]) ||
 
-            (p[0] == player && p[4] == player && p[8] == player) ||
-            (p[6] == player && p[4] == player && p[2] == player))
+            (p[0] == player[0] && p[4] == player[0] && p[8] == player[0]) ||
+            (p[6] == player[0] && p[4] == player[0] && p[2] == player[0]))
             {
                 return 1;
             }
     }
-}
-
-void player_position(char player, char buff[], int i, char n[][3], char p[])
-{
-    if (strcmp(n[i], buff) == 0)
-    {
-        p[i] = player;
-    }
+    return 0;
 }
