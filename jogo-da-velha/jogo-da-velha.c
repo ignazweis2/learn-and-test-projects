134f5c491cefec5dd6746bb01dc94c149c13784b
ignazweis2 <213210679+ignazweis2@users.noreply.github.com>
2025-08-07 22:15:40 -0300

    jogo aparentemente completo agora.

--- jogo-da-velha/jogo-da-velha.c
+++ jogo-da-velha/jogo-da-velha.c
@@ -3,8 +3,9 @@ tornando-o o mais eficiente. Não está completo e não é jogável.*/
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-int calc(char p[], int move, char player[]);
+int calc(char p[], int move, char player);
 
 int main (void)
 {
@@ -17,35 +18,53 @@ int main (void)
     // adiciona um ponto em cada posição do jogo da velha.
     for (int i = 0; i < 9; i++)
     {
-            p[i] = '.';
+        p[i] = '.';
     }
 
     // escolha o jogador.
-    printf("Qual jogador escolhido primeiro? o\\x\n");
+    printf("Qual jogador escolhido primeiro? O \\ X\n");
     printf(": ");
-    char player[2];
-    scanf("%1s", &player);
+    char player;
+    scanf("%1c", &player);
+    player = toupper(player);
 
     // verificar se escolha do jogador é válida
-    if (player[0] != 'x' && player[0] != 'o')
+    if (player != 'X' && player != 'O')
     {
         printf("\nJogador inválido.\n\n");
         return 1;
     }
 
+    printf("\n1a | 1b | 1c\n2a | 2b | 2c\n3a | 3b | 3c\n\n");
+
     while (true)
     {
         // mostre o jogo da velha.
-        printf("%c %c %c\n", p[0], p[1], p[2]);
-        printf("%c %c %c\n", p[3], p[4], p[5]);
-        printf("%c %c %c\n", p[6], p[7], p[8]);
+        printf("   a   b   c\n\n");
+        printf("1   %c | %c | %c\n", p[0], p[1], p[2]);
+        printf("   ---+---+---\n");
+        printf("2   %c | %c | %c\n", p[3], p[4], p[5]);
+        printf("   ---+---+---\n");
+        printf("3   %c | %c | %c\n", p[6], p[7], p[8]);
 
-        printf("\n1a 1b 1c\n2a 2b 2c\n3a 3b 3c\n");
-        printf(": ");
+        printf("\n: ");
+
+        // verificação se alguém ganhou ou perdeu.
+        if (end == 1)
+        {
+            printf("\n%c ganhou!\n", player);
+            break;
+        }
+        else if (move == 8)
+        {
+            printf("\nVocê empatou!\n");
+            break;
+        }
 
         // coloque a opção no buffer.
         char buff[3];
         scanf("%2s", &buff);
+        printf("\n");
 
         // mude o jogo da velha para refletir a opção dada anteriormente.
         char n[9][3] = {"1a","1b","1c","2a","2b","2c","3a","3b","3c"};
@@ -53,54 +72,43 @@ int main (void)
         {
             if (strcmp(n[i], buff) == 0 && p[i] == '.')
             {
-                p[i] = player[0];
+                p[i] = player;
+                move--;
             }
         }
 
         // caso alguém ganhe finalize o while com a variável end.
         end = calc(p, move, player);
 
-        // verificação se alguém ganhou ou perdeu.
-        if (end == 1)
+        // trocar o jogador
+        if (player == 'X')
         {
-            printf("\n\n%c ganhou!\n", player[0]);
-            break;
+            player = 'O';
         }
-        else if (move == 8)
+        else
         {
-            printf("\n\nVocê empatou!\n");
-            break;
+            player = 'X';
         }
 
         // adicionar a quantidade de rodadas
         move++;
-
-        // trocar o jogador
-        if (player[0] == 'x')
-        {
-            player[0] = 'o';
-        }
-        else if (player[0] == 'o')
-        {
-            player[0] = 'x';
-        }
     }
 }
 
-int calc(char p[], int move, char player[])
+int calc(char p[], int move, char player)
 {
     if (move >= 4)
     {
-        if ((p[0] == player[0] && p[1] == player[0] && p[2] == player[0]) ||
-            (p[3] == player[0] && p[4] == player[0] && p[5] == player[0]) ||
-            (p[6] == player[0] && p[7] == player[0] && p[8] == player[0]) ||
+        if ((p[0] == player && p[1] == player && p[2] == player) ||
+            (p[3] == player && p[4] == player && p[5] == player) ||
+            (p[6] == player && p[7] == player && p[8] == player) ||
 
-            (p[0] == player[0] && p[3] == player[0] && p[6] == player[0]) ||
-            (p[1] == player[0] && p[4] == player[0] && p[7] == player[0]) ||
-            (p[2] == player[0] && p[5] == player[0] && p[8] == player[0]) ||
+            (p[0] == player && p[3] == player && p[6] == player) ||
+            (p[1] == player && p[4] == player && p[7] == player) ||
+            (p[2] == player && p[5] == player && p[8] == player) ||
 
-            (p[0] == player[0] && p[4] == player[0] && p[8] == player[0]) ||
-            (p[6] == player[0] && p[4] == player[0] && p[2] == player[0]))
+            (p[0] == player && p[4] == player && p[8] == player) ||
+            (p[6] == player && p[4] == player && p[2] == player))
             {
                 return 1;
             }
