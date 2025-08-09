96199db3246ac048bf1b749bb3f322285d376fae
ignazweis2 <213210679+ignazweis2@users.noreply.github.com>
2025-08-09 17:25:13 -0300

    início do jogo, esboço

---
+++ jogo-de-cobra/snake.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int draw(void);
+
+int main(void)
+{
+    while (true)
+    {
+        draw();
+    }
+    return 0;
+}
+
+int draw(void)
+{
+    printf("\n"); //1
+    printf("\n"); //2
+    printf("\n"); //3
+    printf("\n"); //4
+    printf("\n"); //5
+    printf("\n"); //6
+    printf("\n"); //7
+    printf("\n"); //8
+    printf("\n"); //9
+    printf("\n"); //10
+    printf("\n"); //11
+    printf("\n"); //12
+    printf("\n"); //13
+    printf("\n"); //14
+    printf("\n"); //15
+    printf("\n"); //16
+    printf("\n"); //17
+    printf("\n"); //18
+    printf("\n"); //19
+    printf("\n"); //20
+}
