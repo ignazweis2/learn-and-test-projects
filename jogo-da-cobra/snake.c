be9d4d8d0c0ab09cf94316eebd6399eed8823637
ignazweis2 <213210679+ignazweis2@users.noreply.github.com>
2025-08-16 16:40:39 -0300

    adicionado movimento da cobra como um ponto usando wasd

--- jogo-da-cobra/snake.c
+++ jogo-da-cobra/snake.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ncurses.h>
 
 #define HEIGHT 25
 #define LENGTH 60
@@ -9,12 +10,21 @@ int create_field(char m[HEIGHT][LENGTH]);
 
 int main(void)
 {
+    initscr();
+    cbreak();
+    noecho();
+
     // criar campo inicial com quantidade de caracteres HEIGHT e LENGHT '.' e '#'.
     char m[HEIGHT][LENGTH];
     create_field(m);
 
     int half_height = HEIGHT / 2;
     int half_length = LENGTH / 2;
+
+    int tmp;
+
+    int y = half_height;
+    int x = half_length;
 
     m[half_height][half_length] = 'S';
 
@@ -25,17 +35,45 @@ int main(void)
         {
             for(int j = 0; j < LENGTH; j++)
             {
-                printf("%c", m[i][j]);
+                printw("%c", m[i][j]);
             }
-            printf("\n");
+            printw("\n");
         }
 
-        
+        refresh();
 
-        usleep(500000);
-        system("clear");
-
+        int option = getch();
+        if (option == 'w')
+        {
+            tmp = m[y][x];
+            m[y - 1][x] = tmp;
+            m[y][x] = '.';
+            y = y - 1;
+        }
+        else if (option == 'a')
+        {
+            tmp = m[y][x];
+            m[y][x - 1] = tmp;
+            m[y][x] = '.';
+            x = x - 1;
+        }
+        else if (option == 's')
+        {
+            tmp = m[y][x];
+            m[y + 1][x] = tmp;
+            m[y][x] = '.';
+            y = y + 1;
+        }
+        else if (option == 'd')
+        {
+            tmp = m[y][x];
+            m[y][x + 1] = tmp;
+            m[y][x] = '.';
+            x = x + 1;
+        }
+        clear();
     }
+    endwin();
     return 0;
 }
 
